125f2423ad8d7ecc0732b7fbe7d9758c762d08d6
Ian Ferrier <ian.m.ferrier@gmail.com>
2021-05-04 19:06:41 -0700

    Simple Database Save and Find Test
    database/index.js mongoose simple existance check test made lines 72 through 86.
    Checked connection as well from previous commit and connection works, schema had some changes needed,
    Also during this time I skipped ahead with Jeff on a future ticket to import the csv data,
    So now I have four collections that match the four individual csv files I need seeded into my database.
    Will now be researching aggregrate pipeline for building the main schema for each product.

--- database/index.js
+++ database/index.js
@@ -20,57 +20,70 @@ var productSchema = new mongoose.Schema({
     body: String,
     date: String,
     reviewer_name: String,
-    helpfulness: Number
+    helpfulness: Number,
     photos: [{
      id: Number,
      url: String
     }],
   }],
   reviews_meta: {
-  ratings: {
-    1: String,
-    2: String,
-    3: String,
-    4: String,
-    5: String
-  },
-  recommended: {
-    false: String,
-    true: String
-  },
-  characteristics: {
-    Fit: {
-      id: Number,
-      value: String
+    ratings: {
+      1: String,
+      2: String,
+      3: String,
+      4: String,
+      5: String
     },
-    Length: {
-      id: Number,
-      value: String
+    recommended: {
+      false: String,
+      true: String
     },
-    Comfort: {
-      id: Number,
-      value: String
-    },
-    Quality: {
-      id: Number,
-      value: String
-    },
-    Width: {
-      id: Number,
-      value: String
-    },
-    Size: {
-      id: Number,
-      value: String
+    characteristics: {
+      Fit: {
+        id: Number,
+        value: String
+      },
+      Length: {
+        id: Number,
+        value: String
+      },
+      Comfort: {
+        id: Number,
+        value: String
+      },
+      Quality: {
+        id: Number,
+        value: String
+      },
+      Width: {
+        id: Number,
+        value: String
+      },
+      Size: {
+        id: Number,
+        value: String
+      }
     }
   }
 });
 
 var Product = mongoose.model('Product', productSchema);
 
-var test = new Product({
-  // test document to insert for later
-});
+// // Simple Tests //
+// // Create
+// var test = new Product({
+//   product: '1'
+// });
+// // Save w/ error
+// test.save(err => {
+//   if (err) { return console.error(err) }
+// });
+// // List of all Product documents w/ error
+// Product.find((err, products) => {
+//   if (err) { return console.error(err) }
+//   console.log(products);
+// });
+// // End of Simple Tests //
 
 module.exports = {
   db: db
