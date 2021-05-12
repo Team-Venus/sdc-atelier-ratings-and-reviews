d9e0c027f9f8c925c8dfdfe06674d38467194ce6
Ian Ferrier <ian.m.ferrier@gmail.com>
2021-05-11 20:13:41 -0700

    Fix mongoose connection address.

--- database/init.js
+++ database/init.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/sdc_deploy', {
+mongoose.connect('mongodb://127.0.0.1:27017/sdc_deploy', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
